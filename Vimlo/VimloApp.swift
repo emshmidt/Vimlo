abe5873a951bb88374a9e2b4e950c7b3f01c8b64
Shmidt Emilia <shmidt.em.23@gmail.com>
2026-04-06 13:41:41 +0300

    init

---
+++ Vimlo/VimloApp.swift
@@ -0,0 +1,17 @@
+//
+//  VimloApp.swift
+//  Vimlo
+//
+//  Created by Эмилия Шмидт on 06.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct VimloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
